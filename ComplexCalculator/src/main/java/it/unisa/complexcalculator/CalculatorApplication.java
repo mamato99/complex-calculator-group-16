a85f20ea8982a42c73d88f73fdb576823d7b866f
skane <skane@mbp-di-mdr>
2021-11-23 22:14:54 +0100

    FXML Document modified (added elements in User Interface)

--- ComplexCalculator/src/main/java/it/unisa/complexcalculator/CalculatorApplication.java
+++ ComplexCalculator/src/main/java/it/unisa/complexcalculator/CalculatorApplication.java
@@ -17,8 +17,9 @@ public class CalculatorApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("FXMLDocument"), 640, 480);
+        scene = new Scene(loadFXML("FXMLDocument"));
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- ComplexCalculator/src/main/java/it/unisa/complexcalculator/Controller/FXMLController.java
+++ ComplexCalculator/src/main/java/it/unisa/complexcalculator/Controller/FXMLController.java
@@ -1,6 +1,12 @@
 package it.unisa.complexcalculator.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class FXMLController {
+
+    @FXML
+    private Button primaryButton;
 
     
 }
