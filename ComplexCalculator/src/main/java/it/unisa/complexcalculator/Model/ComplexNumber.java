54e5e83056541e3357f7881ac1c1f1039166e82c
Amministratore <Amministratore@DESKTOP-8T4Q7TS.mshome.net>
2021-11-23 19:25:16 +0100

    ComplexNumber class added

--- ComplexCalculator/src/main/java/it/unisa/complexcalculator/Model/ComplexNumber.java
+++ ComplexCalculator/src/main/java/it/unisa/complexcalculator/Model/ComplexNumber.java
@@ -10,4 +10,69 @@ package it.unisa.complexcalculator.Model;
  */
 public class ComplexNumber {
     
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public ComplexNumber(double real) {
+        this.real = real;
+        this.imaginary = 0;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void setImaginary(double imaginary) {
+        this.imaginary = imaginary;
+    }
+
+    @Override
+    public String toString() {
+        return real + " " + imaginary + "j";
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + (int) (Double.doubleToLongBits(this.real) ^ (Double.doubleToLongBits(this.real) >>> 32));
+        hash = 53 * hash + (int) (Double.doubleToLongBits(this.imaginary) ^ (Double.doubleToLongBits(this.imaginary) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ComplexNumber other = (ComplexNumber) obj;
+        if (Double.doubleToLongBits(this.real) != Double.doubleToLongBits(other.real)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.imaginary) != Double.doubleToLongBits(other.imaginary)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
 }
