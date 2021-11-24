5d6b2abbe13049747b957614f0ad6d42af6f6fc2
Amministratore <Amministratore@DESKTOP-8T4Q7TS.mshome.net>
2021-11-24 18:12:36 +0100

    Label visualized by users updated

--- ComplexCalculator/src/main/java/it/unisa/complexcalculator/Controller/FXMLController.java
+++ ComplexCalculator/src/main/java/it/unisa/complexcalculator/Controller/FXMLController.java
@@ -1,7 +1,10 @@
 package it.unisa.complexcalculator.Controller;
 
+import it.unisa.complexcalculator.Model.ComplexNumber;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 
 public class FXMLController {
@@ -46,53 +49,84 @@ public class FXMLController {
     private Button sqrtButton;
     @FXML
     private Button invButton;
-
     @FXML
-    private void twoClicked(MouseEvent event) {
+    private ListView<ComplexNumber> storedElements;
+    @FXML
+    private Label userLabel;
+
+    private void updateLabel(String to_add) {
+        if ("del".equals(to_add) && userLabel.getText().length() > 0) {
+            if ("Welcome".equals(userLabel.getText())) {
+                userLabel.setText("");
+            } else {
+                userLabel.setText(userLabel.getText().substring(0, userLabel.getText().length() - 1));
+            }
+        } else if ("Welcome".equals(userLabel.getText())) {
+            userLabel.setText(to_add);
+        } else if ("del".equals(to_add)) {
+            userLabel.setText(userLabel.getText() + to_add);
+        }
     }
 
     @FXML
     private void oneClicked(MouseEvent event) {
+        updateLabel("1");
+    }
+
+    @FXML
+    private void twoClicked(MouseEvent event) {
+        updateLabel("2");
     }
 
     @FXML
     private void threeClicked(MouseEvent event) {
+        updateLabel("3");
     }
 
     @FXML
     private void fourClicked(MouseEvent event) {
+        updateLabel("4");
+
     }
 
     @FXML
     private void fiveClicked(MouseEvent event) {
+        updateLabel("5");
     }
 
     @FXML
     private void sixClicked(MouseEvent event) {
+        updateLabel("6");
     }
 
     @FXML
     private void sevenClicked(MouseEvent event) {
+        updateLabel("7");
     }
 
     @FXML
     private void eightClicked(MouseEvent event) {
+        updateLabel("8");
     }
 
     @FXML
     private void nineClicked(MouseEvent event) {
+        updateLabel("9");
     }
 
     @FXML
     private void dotClicked(MouseEvent event) {
+        updateLabel(".");
     }
 
     @FXML
     private void zeroClicked(MouseEvent event) {
+        updateLabel("0");
     }
 
     @FXML
     private void imClicked(MouseEvent event) {
+        updateLabel("i");
     }
 
     @FXML
@@ -101,14 +135,17 @@ public class FXMLController {
 
     @FXML
     private void delClicked(MouseEvent event) {
+        updateLabel("del");
     }
 
     @FXML
     private void plusClicked(MouseEvent event) {
+        updateLabel("+");
     }
 
     @FXML
     private void minusClicked(MouseEvent event) {
+        updateLabel("-");
     }
 
     @FXML
