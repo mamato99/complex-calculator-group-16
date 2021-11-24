aa299d593a203c7939c24e7b38660c04a17686d1
skane <skane@mbp-di-mdr>
2021-11-24 16:51:51 +0100

    Added ids to buttons for Controller and final graphical prototype

--- ComplexCalculator/src/main/java/it/unisa/complexcalculator/Controller/FXMLController.java
+++ ComplexCalculator/src/main/java/it/unisa/complexcalculator/Controller/FXMLController.java
@@ -1,4 +1,129 @@
 package it.unisa.complexcalculator.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class FXMLController {
+
+    @FXML
+    private Button twoButton;
+    @FXML
+    private Button oneButton;
+    @FXML
+    private Button threeButton;
+    @FXML
+    private Button fourButton;
+    @FXML
+    private Button fiveButton;
+    @FXML
+    private Button sixButton;
+    @FXML
+    private Button sevenButton;
+    @FXML
+    private Button eightButton;
+    @FXML
+    private Button nineButton;
+    @FXML
+    private Button dotButton;
+    @FXML
+    private Button zeroButton;
+    @FXML
+    private Button imButton;
+    @FXML
+    private Button insButton;
+    @FXML
+    private Button acButton;
+    @FXML
+    private Button plusButton;
+    @FXML
+    private Button minusButton;
+    @FXML
+    private Button prodButton;
+    @FXML
+    private Button divButton;
+    @FXML
+    private Button sqrtButton;
+    @FXML
+    private Button invButton;
+
+    @FXML
+    private void twoClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void oneClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void threeClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void fourClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void fiveClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void sixClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void sevenClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void eightClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void nineClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void dotClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void zeroClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void imClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void insClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void delClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void plusClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void minusClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void prodClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void divClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void sqrtClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void invClicked(MouseEvent event) {
+    }
 }
