83efe0aecee67910a3ae5ea23d59c16added8059
skane <skane@mbp-di-mdr>
2021-11-28 16:20:12 +0100

    Ultimated tests for NumberMemory class

--- ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/NumberMemoryTest.java
+++ ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/NumberMemoryTest.java
@@ -3,9 +3,7 @@ package it.unisa.complexcalculator.Model;
 import java.util.EmptyStackException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
 
 /**
@@ -23,24 +21,47 @@ public class NumberMemoryTest {
         stack = new NumberMemory();
     }
     
-    @After
-    public void tearDown() {
-    }
-
+    
+    //-----------------------PUSH-----------------------------------------
+    
     /**
-     * Test of push method, of class NumberMemory.
+     * Test of push method, of class NumberMemory. This method pushes a non-null item in the stack.
      */
     @Test
     public void testPush() {
-        System.out.println("push method");
+        System.out.println("push of non-null item method");
         
         ComplexNumber toIns = new ComplexNumber(1,1);
         stack.push(toIns);
         
         assertEquals(stack.len(), 1);
-        assertEquals(stack.pop(), toIns);
+        ComplexNumber popItem = stack.pop();
+        
+        assertEquals(popItem, toIns);
+        
         assertEquals(stack.len(), 0);
     }
+    
+    /**
+     * Test of push method, of class NumberMemory. This method pushes a null item in the stack.
+     */
+    @Test
+    public void testPushNull() {
+        System.out.println("push of null item method");
+        
+        ComplexNumber toIns = null;
+        stack.push(toIns);
+        
+        assertEquals(stack.len(), 1);
+        
+        ComplexNumber popItem = stack.pop();
+        assertNull(popItem);
+        
+        assertEquals(stack.len(), 0);
+    }
+    
+    
+    //-----------------------POP-----------------------------------------
     
     /**
      * Test of exception in pop method, of class NumberMemory.
@@ -53,11 +74,11 @@ public class NumberMemoryTest {
     }
     
     /**
-     * Test of pop method, of class NumberMemory.
+     * Test of pop method, of class NumberMemory. Removing a non-null item.
      */
     @Test
     public void testPop() {
-        System.out.println("pop method");
+        System.out.println("non-null item in pop method");
         
         ComplexNumber toIns = new ComplexNumber(1,1);
 
@@ -66,13 +87,35 @@ public class NumberMemoryTest {
         assertEquals(stack.pop(), toIns);
         assertEquals(stack.len(), 0);
     }
+    
+    
+    /**
+     * Test of pop method, of class NumberMemory. Removing a null item.
+     */
+    @Test
+    public void testPopNull(){
+        System.out.println("null item in pop method");
+        
+        ComplexNumber toIns = null;
 
+        stack.push(toIns);
+        assertEquals(stack.len(), 1);
+        
+        ComplexNumber popItem = stack.pop();
+        assertNull(popItem);
+        
+        assertEquals(stack.len(), 0);
+    }
+    
+    
+    //-----------------------TOP-----------------------------------------
+    
     /**
      * Test of top method, of class NumberMemory.
      */
     @Test
     public void testTop() {
-        System.out.println("top method");
+        System.out.println("non-null item in top method");
         
         ComplexNumber toIns = new ComplexNumber(1,1);
         
@@ -82,15 +125,33 @@ public class NumberMemoryTest {
     }
     
     /**
-     * Test of EmptyStackException in top method, of class NumberMemory.
+     * Test of EmptyStackException in top method, of class NumberMemory. Returning a non-null item.
      */
     @Test(expected = EmptyStackException.class)
     public void testTopEmptyStackException() {
-        System.out.println("top method");
+        System.out.println("EmptyStackException in top method");
         
         stack.top();
     }
 
+    /**
+     * Test of top method, of class NumberMemory. Returning a null item.
+     */
+    @Test
+    public void testTopNull(){
+        System.out.println("null item in top method");
+        
+        ComplexNumber toIns = null;
+
+        stack.push(toIns);
+        assertEquals(stack.len(), 1);
+        
+        assertNull(stack.top());
+                
+    }
+    
+    //-----------------------LEN-----------------------------------------
+    
     /**
      * Test of len method, of class NumberMemory.
      */
@@ -104,30 +165,46 @@ public class NumberMemoryTest {
         stack.pop();
         assertEquals(stack.len(), 0);
     }
-
+    
+    
+    //-----------------------GET STACK-----------------------------------------
+    
     /**
-     * Test of getStack method, of class NumberMemory.
+     * Test of getStack method, of class NumberMemory. Test if the method returns a non-empty stack.
      */
     @Test
     public void testGetStack() {
         System.out.println("getStack");
-        
+
         ComplexNumber c0 = new ComplexNumber(0,0);
         ComplexNumber c1 = new ComplexNumber(1,1);
         ComplexNumber c2 = new ComplexNumber(2,2);
-        
-        ObservableList<ComplexNumber> expectedList_2 = FXCollections.observableArrayList();
-        ObservableList<ComplexNumber> expectedList_1 = FXCollections.observableArrayList(c0, c1, c2);
-        
-        ObservableList<ComplexNumber>  list1 =  stack.getStack();
-        assertEquals(expectedList_2, list1);
-        
+
+        ObservableList<ComplexNumber> expectedList = FXCollections.observableArrayList(c0, c1, c2);
+
         stack.push(c2);
         stack.push(c1);
         stack.push(c0);
         
-        ObservableList<ComplexNumber>  list2 =  stack.getStack();
-        assertEquals(expectedList_1, list2);
+        ObservableList<ComplexNumber>  list =  stack.getStack();
+        assertEquals(expectedList, list);
+
     }
     
+    /**
+     * Test of getStack method, of class NumberMemory. Test if the method returns an empty stack.
+     */
+    @Test
+    public void testGetStackEmpty() {
+        System.out.println("getStack empty");
+
+        ObservableList<ComplexNumber> expectedList = FXCollections.observableArrayList();
+
+        ObservableList<ComplexNumber>  list =  stack.getStack();
+        assertEquals(expectedList, list);
+
+    }
+    
+    
+
 }
