94f2afe8286d3300392a71f172f81b2d46f86f77
skane <skane@mbp-di-mdr>
2021-11-28 18:54:04 +0100

    Added test methods for pushing complex numbers in CalculatorTest class

--- ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/CalculatorTest.java
+++ ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/CalculatorTest.java
@@ -4,42 +4,26 @@
  */
 package it.unisa.complexcalculator.Model;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author Amministratore
+ * @author mdr
  */
 public class CalculatorTest {
-        
-    private Calculator c;
+    private Calculator calc;
+    private ComplexNumber c1,c2;
     
     public CalculatorTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
     @Before
     public void setUp() {
-        c = new Calculator();
-        c.pushNumber(20, 10);
-        c.pushNumber(-10, 8);
-        
-    }
-    
-    @After
-    public void tearDown() {
+        calc = new Calculator();
+        c1=null;
+        c2=null;
     }
 
     /**
@@ -48,53 +32,146 @@ public class CalculatorTest {
     @Test
     public void testGetStoredNumbers() {
         System.out.println("getStoredNumbers");
-        Calculator instance = new Calculator();
-        NumberMemory expResult = null;
-        NumberMemory result = instance.getStoredNumbers();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        NumberMemory result = calc.getStoredNumbers();
+        
+        assertNotNull(result);
+       
     }
-
+    
+    
+    //-----------------------pushNumber_double_double - Boundary analysis-----------------------------------------
+    
     /**
      * Test of pushNumber method, of class Calculator.
      */
     @Test
-    public void testPushNumber_double_double() {
-        System.out.println("pushNumber");
+    public void testPushNumberDoubleDoubleMinValues() {
+        System.out.println("pushNumber - Min values");
+        
+        // Test 1 - Min Values for real and img part
+        double real = -Double.MAX_VALUE;
+        double img = -Double.MAX_VALUE;
+        calc.pushNumber(real, img);
+        assertEquals(new ComplexNumber(real,img), calc.getStoredNumbers().pop());
+        
+    }
+    
+    /**
+     * Test of pushNumber method, of class Calculator.
+     */
+    @Test
+    public void testPushNumberDoubleDoubleMaxValues() {
+        System.out.println("pushNumber - Max values");
+        
+        // Test 2 - Max Values for real and img part
+        double real = Double.MAX_VALUE;
+        double img = Double.MAX_VALUE;
+        calc.pushNumber(real, img);
+        assertEquals(new ComplexNumber(real,img), calc.getStoredNumbers().pop());
+        
+    }
+    
+    /**
+     * Test of pushNumber method, of class Calculator.
+     */
+    @Test
+    public void testPushNumberDoubleDoubleZeroValues() {
+        System.out.println("pushNumber - Zero values");
+        
+        // Test 3 - Zero values for real and img part
         double real = 0.0;
         double img = 0.0;
-        Calculator instance = new Calculator();
-        instance.pushNumber(real, img);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        calc.pushNumber(real, img);
+        assertEquals(new ComplexNumber(real,img), calc.getStoredNumbers().pop());
+        
     }
 
+   
+    //-----------------------pushNumber_String_String - Whitebox testing (path coverage)-----------------------------------------
+    
     /**
      * Test of pushNumber method, of class Calculator.
      */
     @Test
-    public void testPushNumber_String_String() {
+    public void testPushNumberStringStringZeroZero() {
         System.out.println("pushNumber");
-        String re = "";
-        String im = "";
-        Calculator instance = new Calculator();
-        instance.pushNumber(re, im);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String re = "+";
+        String im = "+";
+        
+        calc.pushNumber(re, im);
+        ComplexNumber expResult = new ComplexNumber(0, 0);
+        
+        ComplexNumber result = calc.getStoredNumbers().pop();
+        
+        assertEquals(expResult, result);
     }
-
+    
+    /**
+     * Test of pushNumber method, of class Calculator.
+     */
+    @Test
+    public void testPushNumberStringStringNonZeroNonZero() {
+        System.out.println("pushNumber");
+        String re = "+1.0";
+        String im = "+1.0";
+        
+        calc.pushNumber(re, im);
+        ComplexNumber expResult = new ComplexNumber(1, 1);
+        
+        ComplexNumber result = calc.getStoredNumbers().pop();
+        
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of pushNumber method, of class Calculator.
+     */
+    @Test
+    public void testPushNumberStringStringZeroNonZero() {
+        System.out.println("pushNumber");
+        String re = "+0.0";
+        String im = "+1.0";
+        
+        calc.pushNumber(re, im);
+        ComplexNumber expResult = new ComplexNumber(0, 1);
+        
+        ComplexNumber result = calc.getStoredNumbers().pop();
+        
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of pushNumber method, of class Calculator.
+     */
+    @Test
+    public void testPushNumberStringStringNonZeroZero() {
+        System.out.println("pushNumber");
+        String re = "+1.0";
+        String im = "+0.0";
+        
+        calc.pushNumber(re, im);
+        ComplexNumber expResult = new ComplexNumber(1, 0);
+        
+        ComplexNumber result = calc.getStoredNumbers().pop();
+        
+        assertEquals(expResult, result);
+    }
+    //-----------------------add-----------------------------------------
+    
     /**
      * Test of add method, of class Calculator.
      */
     @Test
     public void testAdd() {
         System.out.println("add");
-        ComplexNumber expected = ComplexOperations.add(new ComplexNumber(20,10), new ComplexNumber(-10,8));
-        c.add();
-        assertEquals(expected, c.getStoredNumbers().top());
+        Calculator instance = new Calculator();
+        instance.add();
     }
-
+    
+    
+    //-----------------------subtract-----------------------------------------
+    
     /**
      * Test of subtract method, of class Calculator.
      */
@@ -103,10 +180,12 @@ public class CalculatorTest {
         System.out.println("subtract");
         Calculator instance = new Calculator();
         instance.subtract();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
+    
+    
+    
+    //-----------------------subtract-----------------------------------------
+    
     /**
      * Test of divide method, of class Calculator.
      */
@@ -115,10 +194,12 @@ public class CalculatorTest {
         System.out.println("divide");
         Calculator instance = new Calculator();
         instance.divide();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
+    
+    
+    
+    //-----------------------multiply-----------------------------------------
+    
     /**
      * Test of multiply method, of class Calculator.
      */
@@ -127,10 +208,12 @@ public class CalculatorTest {
         System.out.println("multiply");
         Calculator instance = new Calculator();
         instance.multiply();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
+    
+    
+    
+    //-----------------------invertSign-----------------------------------------
+    
     /**
      * Test of invertSign method, of class Calculator.
      */
@@ -139,10 +222,12 @@ public class CalculatorTest {
         System.out.println("invertSign");
         Calculator instance = new Calculator();
         instance.invertSign();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
+    
+    
+    
+    //-----------------------squareRoot-----------------------------------------
+    
     /**
      * Test of squareRoot method, of class Calculator.
      */
@@ -151,8 +236,6 @@ public class CalculatorTest {
         System.out.println("squareRoot");
         Calculator instance = new Calculator();
         instance.squareRoot();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
