a5aa7ff9357cbedfb87e28164305e2fb1804b4d5
MarioAmato99 <super@DESKTOP-VLLOSAK>
2021-11-28 19:09:43 +0100

    Added test methods for operations in CalculatorTest class

--- ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/CalculatorTest.java
+++ ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/CalculatorTest.java
@@ -4,42 +4,28 @@
  */
 package it.unisa.complexcalculator.Model;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import it.unisa.complexcalculator.Exception.OutOfBoundException;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author Amministratore
+ * @author mdr
  */
 public class CalculatorTest {
-        
-    private Calculator c;
-    
+
+    private Calculator calc;
+    private ComplexNumber c1, c2;
+
     public CalculatorTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
     @Before
     public void setUp() {
-        c = new Calculator();
-        c.pushNumber(20, 10);
-        c.pushNumber(-10, 8);
-        
-    }
-    
-    @After
-    public void tearDown() {
+        calc = new Calculator();
+        c1 = null;
+        c2 = null;
     }
 
     /**
@@ -48,28 +34,60 @@ public class CalculatorTest {
     @Test
     public void testGetStoredNumbers() {
         System.out.println("getStoredNumbers");
-        Calculator instance = new Calculator();
-        NumberMemory expResult = null;
-        NumberMemory result = instance.getStoredNumbers();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        NumberMemory result = calc.getStoredNumbers();
+
+        assertNotNull(result);
+
+    }
+
+    //-----------------------pushNumber_double_double-----------------------------------------
+    /**
+     * Test of pushNumber method, of class Calculator.
+     */
+    @Test
+    public void testPushNumber_double_doubleMinValues() {
+        System.out.println("pushNumber - Min values");
+
+        // Test 1 - Min Values for real and img part
+        double real = -Double.MAX_VALUE;
+        double img = -Double.MAX_VALUE;
+        calc.pushNumber(real, img);
+        assertEquals(new ComplexNumber(real, img), calc.getStoredNumbers().pop());
+
     }
 
     /**
      * Test of pushNumber method, of class Calculator.
      */
     @Test
-    public void testPushNumber_double_double() {
-        System.out.println("pushNumber");
-        double real = 0.0;
-        double img = 0.0;
-        Calculator instance = new Calculator();
-        instance.pushNumber(real, img);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testPushNumber_double_doubleMaxValues() {
+        System.out.println("pushNumber - Max values");
+
+        // Test 2 - Max Values for real and img part
+        double real = Double.MAX_VALUE;
+        double img = Double.MAX_VALUE;
+        calc.pushNumber(real, img);
+        assertEquals(new ComplexNumber(real, img), calc.getStoredNumbers().pop());
+
     }
 
+    /**
+     * Test of pushNumber method, of class Calculator.
+     */
+    @Test
+    public void testPushNumber_double_doubleZeroValues() {
+        System.out.println("pushNumber - Zero values");
+
+        // Test 3 - Zero values for real and img part
+        double real = 0.0;
+        double img = 0.0;
+        calc.pushNumber(real, img);
+        assertEquals(new ComplexNumber(real, img), calc.getStoredNumbers().pop());
+
+    }
+
+    //-----------------------pushNumber_String_String-----------------------------------------
     /**
      * Test of pushNumber method, of class Calculator.
      */
@@ -80,79 +98,416 @@ public class CalculatorTest {
         String im = "";
         Calculator instance = new Calculator();
         instance.pushNumber(re, im);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+
+    //-----------------------add-----------------------------------------
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @Test
+    public void testAddMaxValues() {
+        System.out.println("add");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.add(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.add();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
     }
 
     /**
      * Test of add method, of class Calculator.
      */
     @Test
-    public void testAdd() {
+    public void testAddMinValues() {
         System.out.println("add");
-        ComplexNumber expected = ComplexOperations.add(new ComplexNumber(20,10), new ComplexNumber(-10,8));
-        c.add();
-        assertEquals(expected, c.getStoredNumbers().top());
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.add(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.add();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @Test
+    public void testAddZeroValues() {
+        System.out.println("add");
+        c1 = new ComplexNumber(0, 0);
+        c2 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.add(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.add();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testAddOutOfBoundExceptionMax() {
+        System.out.println("add");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.add();
+    }
+
+    /**
+     * Test of add method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testAddOutOfBoundExceptionMin() {
+        System.out.println("add");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.add();
+    }
+
+    //-----------------------subtract-----------------------------------------
+    /**
+     * Test of subtract method, of class Calculator.
+     */
+    @Test
+    public void testSubtractMaxValues() {
+        System.out.println("Subtract");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.difference(c1, c2);
+        calc.getStoredNumbers().push(c2);
+        calc.getStoredNumbers().push(c1);
+        calc.subtract();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
     }
 
     /**
      * Test of subtract method, of class Calculator.
      */
     @Test
-    public void testSubtract() {
-        System.out.println("subtract");
-        Calculator instance = new Calculator();
-        instance.subtract();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSubtractMinValues() {
+        System.out.println("Subtract");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.difference(c1, c2);
+        calc.getStoredNumbers().push(c2);
+        calc.getStoredNumbers().push(c1);
+        calc.subtract();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of subtract method, of class Calculator.
+     */
+    @Test
+    public void testSubtractZeroValues() {
+        System.out.println("Subtract");
+        c1 = new ComplexNumber(0, 0);
+        c2 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.difference(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.subtract();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of subtract method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testSubtractOutOfBoundExceptionMax() {
+        System.out.println("Subtract");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.subtract();
+    }
+
+    /**
+     * Test of subtract method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testSubtractOutOfBoundExceptionMin() {
+        System.out.println("Subtract");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.subtract();
+    }
+
+    //-----------------------divide-----------------------------------------
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @Test
+    public void testDividetMaxValues() {
+        System.out.println("divide");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(1, 0);
+        ComplexNumber expResult = ComplexOperations.division(c1, c2);
+        calc.getStoredNumbers().push(c2);
+        calc.getStoredNumbers().push(c1);
+        calc.divide();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
     }
 
     /**
      * Test of divide method, of class Calculator.
      */
     @Test
-    public void testDivide() {
+    public void testDivideMinValues() {
         System.out.println("divide");
-        Calculator instance = new Calculator();
-        instance.divide();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(1, 0);
+        ComplexNumber expResult = ComplexOperations.division(c1, c2);
+        calc.getStoredNumbers().push(c2);
+        calc.getStoredNumbers().push(c1);
+        calc.divide();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @Test
+    public void testDivideWithDividendZero() {
+        System.out.println("divide");
+        c1 = new ComplexNumber(0, 0);
+        c2 = new ComplexNumber(1, 1);
+        ComplexNumber expResult = ComplexOperations.division(c1, c2);
+        calc.getStoredNumbers().push(c2);
+        calc.getStoredNumbers().push(c1);
+        calc.divide();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @Test(expected = ArithmeticException.class)
+    public void testDivideForZero() {
+        System.out.println("divide");
+        c1 = new ComplexNumber(0, 0);
+        c2 = new ComplexNumber(1, 1);
+        ComplexNumber expResult = ComplexOperations.division(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.divide();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testDivideOutOfBoundExceptionMax() {
+        System.out.println("divide");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(1.0/Double.MAX_VALUE, 1.0/Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.divide();
+    }
+
+    /**
+     * Test of divide method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testDivideOutOfBoundExceptionMin() {
+        System.out.println("Multiply");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(1.0/Double.MAX_VALUE, 1.0/Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.divide();
+    }
+
+    //-----------------------multiply-----------------------------------------
+    /**
+     * Test of multiply method, of class Calculator.
+     */
+    @Test
+    public void testMultiplytMaxValues() {
+        System.out.println("Multiply");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(1.0, 0);
+        ComplexNumber expResult = ComplexOperations.multiplication(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.multiply();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
     }
 
     /**
      * Test of multiply method, of class Calculator.
      */
     @Test
-    public void testMultiply() {
-        System.out.println("multiply");
-        Calculator instance = new Calculator();
-        instance.multiply();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testMultiplyMinValues() {
+        System.out.println("Multiply");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(1, 0);
+        ComplexNumber expResult = ComplexOperations.multiplication(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.multiply();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of multiply method, of class Calculator.
+     */
+    @Test
+    public void testMultiplyZeroValues() {
+        System.out.println("Multiply");
+        c1 = new ComplexNumber(0, 0);
+        c2 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.multiplication(c1, c2);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.multiply();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of multiply method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testMultiplyOutOfBoundExceptionMax() {
+        System.out.println("Multiply");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        c2 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.multiply();
+    }
+
+    /**
+     * Test of multiply method, of class Calculator.
+     */
+    @Test(expected = OutOfBoundException.class)
+    public void testMultiplyOutOfBoundExceptionMin() {
+        System.out.println("Multiply");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        c2 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        calc.getStoredNumbers().push(c1);
+        calc.getStoredNumbers().push(c2);
+        calc.multiply();
+    }
+
+    //-----------------------invertSign-----------------------------------------
+    
+    /**
+     * Test of invertSign method, of class Calculator.
+     */
+    @Test
+    public void testInvertSignMaxValue() {
+        System.out.println("invertSign");
+        c1 = new ComplexNumber(Double.MAX_VALUE, Double.MAX_VALUE);
+        ComplexNumber expResult = ComplexOperations.signInversion(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.invertSign();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
     }
 
     /**
      * Test of invertSign method, of class Calculator.
      */
     @Test
-    public void testInvertSign() {
+    public void testInvertSignMinValue() {
         System.out.println("invertSign");
-        Calculator instance = new Calculator();
-        instance.invertSign();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        c1 = new ComplexNumber(-Double.MAX_VALUE, -Double.MAX_VALUE);
+        ComplexNumber expResult = ComplexOperations.signInversion(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.invertSign();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of invertSign method, of class Calculator.
+     */
+    @Test
+    public void testInvertSignZeroValue() {
+        System.out.println("invertSign");
+        c1 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.signInversion(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.invertSign();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    //-----------------------squareRoot-----------------------------------------
+    /**
+     * Test of squareRoot method, of class Calculator.
+     */
+    @Test
+    public void testSquareRootPosNeg() {
+        System.out.println("squareRoot");
+        c1 = new ComplexNumber(1,-1);
+        ComplexNumber expResult = ComplexOperations.squareRoot(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.squareRoot();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
     }
 
     /**
      * Test of squareRoot method, of class Calculator.
      */
     @Test
-    public void testSquareRoot() {
+    public void testSquareRootNegPos() {
         System.out.println("squareRoot");
-        Calculator instance = new Calculator();
-        instance.squareRoot();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        c1 = new ComplexNumber(-1, 1);
+        ComplexNumber expResult = ComplexOperations.squareRoot(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.squareRoot();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
     }
-    
+
+        /**
+     * Test of squareRoot method, of class Calculator.
+     */
+    @Test
+    public void testSquareRootPosPos() {
+        System.out.println("squareRoot");
+        c1 = new ComplexNumber(1, 1);
+        ComplexNumber expResult = ComplexOperations.squareRoot(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.squareRoot();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+        /**
+     * Test of squareRoot method, of class Calculator.
+     */
+    @Test
+    public void testSquareRootNegNeg() {
+        System.out.println("squareRoot");
+        c1 = new ComplexNumber(-1, -1);
+        ComplexNumber expResult = ComplexOperations.squareRoot(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.squareRoot();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
+    /**
+     * Test of squareRoot method, of class Calculator.
+     */
+    @Test
+    public void testSquareRootZeroValues() {
+        System.out.println("squareRoot");
+        c1 = new ComplexNumber(0, 0);
+        ComplexNumber expResult = ComplexOperations.squareRoot(c1);
+        calc.getStoredNumbers().push(c1);
+        calc.squareRoot();
+        assertEquals(expResult, calc.getStoredNumbers().pop());
+    }
+
 }
