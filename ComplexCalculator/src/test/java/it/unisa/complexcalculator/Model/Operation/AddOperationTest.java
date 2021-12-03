fbfd6622cab803e7caa8674fdfadb87ecf8195fa
skane <skane@mbp-di-mdr>
2021-12-03 16:35:43 +0100

    Test template

--- ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/Operation/AddOperationTest.java
+++ ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/Operation/AddOperationTest.java
@@ -4,6 +4,10 @@
  */
 package it.unisa.complexcalculator.Model.Operation;
 
+import it.unisa.complexcalculator.Exception.NotEnoughOperandsException;
+import it.unisa.complexcalculator.Model.Calculator;
+import it.unisa.complexcalculator.Model.ComplexNumber;
+import it.unisa.complexcalculator.Model.ComplexOperations;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,12 +18,14 @@ import static org.junit.Assert.*;
  * @author mdr
  */
 public class AddOperationTest {
+    private Calculator c;
     
     public AddOperationTest() {
     }
     
     @Before
     public void setUp() {
+        c = new Calculator();
     }
     
     @After
@@ -32,10 +38,30 @@ public class AddOperationTest {
     @Test
     public void testExecute() {
         System.out.println("execute");
-        AddOperation instance = null;
+        AddOperation instance = new AddOperation(c);
+        
+        ComplexNumber c1 = new ComplexNumber(1,1);
+        ComplexNumber c2 = new ComplexNumber(1,1);
+        
+        c.getStoredNumbers().push(c1);
+        c.getStoredNumbers().push(c2);
+        
         instance.execute();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        assertEquals(c.getStoredNumbers().pop(), ComplexOperations.add(c1, c2));
+        
+    }
+    
+    /**
+     * Test of execute method, of class AddOperation.
+     */
+    @Test(expected = NotEnoughOperandsException.class)
+    public void testExecuteNotEnoughOperandsExeption() {
+        System.out.println("execute");
+        AddOperation instance = new AddOperation(c);
+        
+        instance.execute();
+        
     }
     
 }
