277549569ca46c92beaab0e687d2be87f8e863bb
Amministratore <Amministratore@DESKTOP-8T4Q7TS>
2021-12-07 16:51:59 +0100

    Third Step of refactoring

---
+++ ComplexCalculator/src/test/java/it/unisa/complexcalculator/Model/Operation/VariableOperation/StackToVariableOperationTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package it.unisa.complexcalculator.Model.Operation.VariableOperation;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Amministratore
+ */
+public class StackToVariableOperationTest {
+    
+    public StackToVariableOperationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of execute method, of class StackToVariableOperation.
+     */
+    @Test
+    public void testExecute() {
+        System.out.println("execute");
+        StackToVariableOperation instance = null;
+        instance.execute();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
